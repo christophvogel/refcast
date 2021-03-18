fedb8ebb7d3dd5c9b2cf2ded02cb815b89970599
christophvogel <christoph@boerd.net>
2021-03-18 17:53:57 +0100

    ♻️  Refactor issues from last PR

--- components/SkipLogin/SkipLoginButton.tsx
+++ components/SkipLogin/SkipLoginButton.tsx
@@ -1,14 +1,14 @@
 import styles from "./SkipLoginButton.module.css";
 
 function SkipLoginButton() {
-  function handleClick(e) {
+  function handleClick() {
     alert("This feature will be available in a later version of the app.");
   }
 
   return (
-    <a href="#" onClick={handleClick}>
-      <button className={styles.skip}>Skip Login and just Browse</button>
-    </a>
+    <button onClick={handleClick} className={styles.skip}>
+      Skip Login and just Browse
+    </button>
   );
 }
 export default SkipLoginButton;
