c95c74fa6b5213ea29d2873a13925e16c1c7f3a0
christophvogel <christoph@boerd.net>
2021-03-24 17:28:14 +0100

    ✨   Update _app.tsx to get token

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,30 @@
 import type { AppProps } from "next/app";
+import { useEffect, useState } from "react";
+import SpotifyWebApi from "spotify-web-api-js";
+import { getTokenFromUrl } from "../components/Login/SpotifyAuth";
 import "../styles/globals.css";
 
+const spotify = new SpotifyWebApi();
+
 function MyApp({ Component, pageProps }: AppProps) {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+      spotify.setAccessToken(_token);
+      spotify.getMe().then((user) => {
+        console.log("Username:", user);
+      });
+    }
+
+    console.log("THE TOKEN IS:", _token);
+  }, []);
   return <Component {...pageProps} />;
 }
 
