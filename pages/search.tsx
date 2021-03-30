ed5530a05fd10db56b863f16452f12c518c007ee
christophvogel <christoph@boerd.net>
2021-03-30 10:43:23 +0200

    ⚰️  Delete unnecessary code

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,31 +1,13 @@
-import { useState } from "react";
 import Logo from "../components/Logo/Logo";
 import SearchEpisode from "../components/SearchEpisode/SearchEpisode";
-import { useSpotify } from "../contexts/SpotifyContext";
 
 import styles from "../styles/Home.module.css";
 
 function SearchPage() {
-  const spotify = useSpotify();
-  const [
-    searchResult,
-    setSearchResult,
-  ] = useState<SpotifyApi.EpisodeSearchResponse>(null);
-
   return (
     <main className={styles.main}>
       <Logo size="small"></Logo>
       <SearchEpisode></SearchEpisode>
-      <button
-        onClick={() => spotify.searchEpisodes("Spiegel").then(setSearchResult)}
-      >
-        Search now!
-      </button>
-      <ul>
-        {searchResult?.episodes.items.map((item) => (
-          <li key={item.id}>{item.name}</li>
-        ))}
-      </ul>
     </main>
   );
 }
