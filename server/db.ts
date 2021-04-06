72283ac3b38d4f65d89448ec2023311e70f27e38
christophvogel <christoph@boerd.net>
2021-04-06 17:50:56 +0200

    ✨  Add functions to db.ts

--- server/db.ts
+++ server/db.ts
@@ -1,23 +1,27 @@
 import { MongoClient } from "mongodb";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export type Reference = {
-  id: number;
-  reference: string;
-  references: string[];
-};
-
 let client = null;
 let db = null;
 
 const url = process.env.MONGODB_URL;
+
+export type PodcastEntry = {
+  _id: string;
+  spotify_id: string;
+  references: Reference[];
+};
+export type Reference = {
+  name: string;
+  timecode: string;
+};
 
 type Handler = (req: NextApiRequest, res: NextApiResponse) => void;
 export const withDatabase = (handler: Handler) => async (
   req: NextApiRequest,
   res: NextApiResponse
 ) => {
-  await connectDB(url, "reference_example");
+  await connectDB(url, "refcast");
   return await handler(req, res);
 };
 
@@ -33,6 +37,34 @@ export async function getCollection(collectioName) {
   return await db.collection(collectioName);
 }
 
+export async function getRefs(collectioName) {
+  return await db.collection(collectioName).find().toArray();
+}
+
+export async function createRefs(newRef) {
+  const orderCollection = await getCollection("reference_example");
+  return await orderCollection.insertOne(newRef);
+}
+
 export function closeDB() {
   client.close();
+}
+
+export async function createPodcastEntry(spotifyId) {
+  const podcastCollection = await getCollection("reference_example");
+  return await podcastCollection.insertOne({
+    spotify_id: spotifyId,
+    references: [],
+  });
+}
+
+export async function getPodcastEntry(spotifyId) {
+  const podcastCollection = await getCollection("reference_example");
+  const podcastEntry = await podcastCollection.findOne({
+    spotify_id: spotifyId,
+  });
+  if (!podcastEntry) {
+    return null;
+  }
+  return podcastEntry;
 }
